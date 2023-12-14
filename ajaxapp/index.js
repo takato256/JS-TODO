b4938a930e01451183568400acf1eb033dde5cc2
takato256 <c1139952@planet.kanazawa-it.ac.jp>
2023-12-14 22:24:42 +0900

    [変更] 関数の分割

--- ajaxapp/index.js
+++ ajaxapp/index.js
@@ -1,3 +1,7 @@
+function main(){
+    fetchUserInfo("takato256");
+}
+
 function fetchUserInfo(userId){
     fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
         .then(response => {
@@ -7,24 +11,27 @@ function fetchUserInfo(userId){
             } else {
                 return response.json().then(userInfo => {
                     // HTMLの組み立て
-                    const view = escapeHTML`
-                    <h4>${userInfo.name} (@${userInfo.login})</h4>
-                    <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
-                    <dl>
-                        <dt>Location</dt>
-                        <dd>${userInfo.location}</dd>
-                        <dt>Reposotories</dt>
-                        <dd>${userInfo.public_repos}</dd>
-                    </dl>
-                    `;
+                    const view = createView(userInfo);
                     // HTMLの挿入
-                    const result = document.getElementById("result");
-                    result.innerHTML = view;
+                    displayView(view);
                 });
             }
         }).catch(error => {
             console.error(error);
         });
+}
+
+function createView(userInfo){
+    return escapeHTML`
+    <h4>${userInfo.name} (@${userInfo.login})</h4>
+    <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+    <dl>
+        <dt>Location</dt>
+        <dd>${userInfo.location}</dd>
+        <dt>Reposotories</dt>
+        <dd>${userInfo.public_repos}</dd>
+    </dl>
+    `;
 }
 
 function escapeSpecialChars(str){
@@ -34,6 +41,11 @@ function escapeSpecialChars(str){
         .replace(/>/g, "&gt;")
         .replace(/"/g, "&quot;")
         .replace(/'/g, "&#039;");
+}
+
+function displayView(view){
+    const result = document.getElementById("result");
+    result.innerHTML = view;
 }
 
 function escapeHTML(strings, ...values){
