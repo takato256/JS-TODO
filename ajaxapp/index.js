173f421b1a479a8968232ba65047b24d1a66b777
takato256 <c1139952@planet.kanazawa-it.ac.jp>
2023-12-14 19:59:48 +0900

    Fetch API 実装

--- ajaxapp/index.js
+++ ajaxapp/index.js
@@ -1,1 +1,16 @@
-console.log("index.js: loaded");
+function fetchUserInfo(userId){
+    fetch(`https://apt.github.com/users/${encodeURIComponent(userId)}`)
+        .then(response => {
+            console.log(response.status);
+            // エラーレスポンスが返されたことを検知する
+            if (!response.ok) {
+                console.log("エラーレスポンス", response);
+            } else {
+                return response.json().then(userInfo => {
+                    console.log(userInfo);
+                });
+            }
+        }).catch(error => {
+            console.error(error);
+        });
+}
