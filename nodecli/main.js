8f2ff29df27d0b4ba2a1f07f180dfc6aa1f0a4d6
takato256 <c1139952@planet.kanazawa-it.ac.jp>
2023-12-15 12:01:42 +0900

    [変更] エラーハンドリング処理

--- nodecli/main.js
+++ nodecli/main.js
@@ -1,9 +1,18 @@
 // commanderモジュールからprogramオブジェクトをインポートする
 import { program } from "commander";
+// fs/primisesモジュールをfsオブジェクトとしてインポートする
+import * as fs from "node:fs/promises";
 
 // コマンドライン引数をcommanderでパースする
 program.parse(process.argv);
-
-// ファイルパスをprogram.args配列から取り出す
+// コマンドライン引数からファイルパスを取得する
 const filePath = program.args[0];
-console.log(filePath);
+
+// ファイルを非同期で読み込む
+fs.readFile(filePath, { encoding: "utf-8" }).then(file => {
+    console.log(file);
+}).catch(err => {
+    console.error(err.message);
+    // 終了ステータス1(一般的なエラー)としてプロセスを終了する
+    process.exit(1);
+});
