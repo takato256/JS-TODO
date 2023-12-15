ec60d593e3bbd48fa2ee82412225d4ceac1ebd2d
takato256 <c1139952@planet.kanazawa-it.ac.jp>
2023-12-15 20:50:40 +0900

    [追加] markedパッケージ

--- nodecli/main.js
+++ nodecli/main.js
@@ -2,6 +2,8 @@
 import { program } from "commander";
 // fs/primisesモジュールをfsオブジェクトとしてインポートする
 import * as fs from "node:fs/promises";
+// markedモジュールからmarkedオブジェクトをインポートする
+import { marked } from "marked";
 
 // コマンドライン引数をcommanderでパースする
 program.parse(process.argv);
@@ -10,7 +12,9 @@ const filePath = program.args[0];
 
 // ファイルを非同期で読み込む
 fs.readFile(filePath, { encoding: "utf-8" }).then(file => {
-    console.log(file);
+    // MarkdownファイルをHTML文字列に変換する
+    const html = marked.parse(file);
+    console.log(html);
 }).catch(err => {
     console.error(err.message);
     // 終了ステータス1(一般的なエラー)としてプロセスを終了する
