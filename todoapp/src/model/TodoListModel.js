fbc63907c84d202b9cf484104cde3ac62b750dca
takato256 <c1139952@planet.kanazawa-it.ac.jp>
2023-12-18 22:46:25 +0900

    [追加] Todoアイテムを配列に追加する

--- todoapp/src/model/TodoListModel.js
+++ todoapp/src/model/TodoListModel.js
@@ -20,6 +20,35 @@ export class TodoListModel extends EventEmitter {
     }
 
     /**
-     * 
+     * 表示できるTodoItemの配列を返す
+     * @returns {TodoItemModel[]}
      */
+    getTodoItems() {
+        return this.#items;
+    }
+
+    /**
+     * TodoListの状態が更新されたときに呼び出されるリスナー関数を登録する
+     * @param {Function} listener
+     */
+    onChange(listener) {
+        this.addEventListener("change", listener);
+    }
+
+    /**
+     * 状態が変更されたときに呼ぶ。登録済みのリスナー関数を呼び出す
+     */
+    emitChange() {
+        this.emit("change");
+    }
+
+    /**
+     * TodoItemを追加する
+     * @param {TodoItemModel} todoItem
+     */
+    addTodo(todoItem) {
+        this.#items.push(todoItem);
+        this.emitChange();
+    }
+
 }
